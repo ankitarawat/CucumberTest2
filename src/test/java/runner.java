c62a50629c655ed832c0c80850a6a91d3cb5af02
Administrator <Administrator@172.31.30.135>
2017-09-27 10:52:18 +0000

    updating runner again

--- src/test/java/runner.java
+++ src/test/java/runner.java
@@ -1,9 +1,9 @@
 import org.junit.runner.RunWith;
-import org.junit.runner.RunWith; 
+//import org.junit.runner.RunWith; 
 import cucumber.junit.Cucumber; 
 
 @RunWith(Cucumber.class)
 
-@Cucumber.Options(format={"pretty","html:reports/test-report", "json:target/cucumber.json"},tags= "@smokeTest")
+@Cucumber.Options(format={"pretty", "html:target/cucumber","json:target/cucumber.json"},tags= "@smokeTest")
 public class runner {
 }
